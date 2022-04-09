18087664146596535ff1129804d50c5b0140f003
Rupesh <rupeshkataria30@gmail.com>
2022-04-09 16:41:14 +0530

    added a  welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -2,4 +2,6 @@ from thinkter import Tk,Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600*300")
+window.configure(bg="steelblue")
+label=Label(window,text="Welcome!")
 window.mainloop()
